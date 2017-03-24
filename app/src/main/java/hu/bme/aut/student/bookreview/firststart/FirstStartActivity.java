e4605e1f7c990bc28293bdc3f4ecba49551c1547
Dániel Zolnai <daniel@egeniq.com>
2017-03-24 09:39:57 +0100

    Fix injection

--- app/src/main/java/hu/bme/aut/student/bookreview/firststart/FirstStartActivity.java
+++ app/src/main/java/hu/bme/aut/student/bookreview/firststart/FirstStartActivity.java
@@ -3,10 +3,16 @@ package hu.bme.aut.student.bookreview.firststart;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 import dagger.android.AndroidInjection;
 import hu.bme.aut.student.bookreview.R;
+import hu.bme.aut.student.bookreview.model.service.SettingsService;
 
 public class FirstStartActivity extends AppCompatActivity {
+
+    @Inject
+    /* package */ SettingsService _settingsService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/hu/bme/aut/student/bookreview/inject/ApplicationComponent.java
+++ app/src/main/java/hu/bme/aut/student/bookreview/inject/ApplicationComponent.java
@@ -8,7 +8,7 @@ import hu.bme.aut.student.bookreview.inject.submodule.FirstStartActivityModule;
  * The main application component which does the injections.
  * Created by Dani on 2017-03-24.
  */
-@Component(modules = { FirstStartActivityModule.class })
+@Component(modules = {ApplicationModule.class, FirstStartActivityModule.class })
 public interface ApplicationComponent {
     void inject(BookReviewApplication application);
 }
